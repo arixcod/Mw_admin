68c346ba802b51d5a7f826d16dbdfddcf17ffa74
arixcod <anandmishra8009@gmail.com>
2024-06-08 20:41:23 +0530

    Added default Booking Page

---
+++ lib/commons/default_booking_page.dart
@@ -0,0 +1,103 @@
+import 'package:admin/admin_page.dart';
+import 'package:admin/utils/constatnts/Image_strings.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/constatnts/colors.dart';
+
+
+class DefaultBookingPage extends StatelessWidget {
+  const DefaultBookingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Tcolors.scaffoldclr_admin,
+      appBar: AppBar(
+                    toolbarHeight: 80,
+                    elevation: 0,
+                    backgroundColor: const Color.fromRGBO(247, 246, 240, 1),
+                    leading: null,
+                    automaticallyImplyLeading: false, 
+                    title: Row(
+                    children: [
+                     Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(8.0),
+                       border: Border.all(color:const Color(0XFFD9D9D9))   
+                      ),
+                      child:const Center(child: Icon(Icons.arrow_back)),
+                     ),
+
+                   const  SizedBox(width: 20,), 
+                      const Text('My Bookings',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold
+                      ),),
+                    ],
+                  ),
+ 
+ 
+                ),
+ 
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+          child: 
+              custom_card(
+               height: MediaQuery.of(context).size.height/2, 
+                child: Column(
+              
+                children: [
+                //use Your Own Image           
+              const SizedBox(height: 20,),
+              Image.asset(TImages.defualt_scan_qr),
+              const SizedBox(height: 20,),
+               
+              const  Text("No Lockers Booked Yet"
+                ,style: 
+                  TextStyle(
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    letterSpacing: 0.1
+                  )
+                ,),   
+                const SizedBox(height: 20,),
+                
+                           const   Text("Book Your Favourite Locker Through Book Locker",style: 
+                  TextStyle(
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    letterSpacing: 0.1
+                  )),
+              
+                const SizedBox(height: 20,),
+                        
+                ElevatedButton(
+                  
+                  style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 25,vertical: 10),
+                  backgroundColor: Tcolors.textheadlineclr,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    side: BorderSide.none
+                    
+                  )
+                  ),
+                  onPressed: 
+                 (){},child:Text(" GO TO BOOK LOCKER",style: 
+                  TextStyle(
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                    letterSpacing: 0.1
+                  )))
+                ],
+              )) ),
+        
+    );
+  }
+}
--- lib/utils/constatnts/Image_strings.dart
+++ lib/utils/constatnts/Image_strings.dart
@@ -7,6 +7,8 @@ class TImages {
   static const String book_locker = "assets/icons/ic_bottom_locker.svg";
   static const String search_bar = "assets/icons/ic_search.svg";
   static const String scan_qr = "assets/icons/ic_scan.svg";
-
-
+  
+  // -default Image
+  static const String defualt_scan_qr = "assets/qr.png";
+  
 }
